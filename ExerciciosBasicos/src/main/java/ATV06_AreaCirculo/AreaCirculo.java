00e79fffa49601902bf1fde76af374e5e7060413
Maycon123 <>
2025-04-26 19:23:10 -0300

    atividade de calculo da area de um circulo

--- ExerciciosBasicos/src/main/java/ATV06_AreaCirculo/AreaCirculo.java
+++ ExerciciosBasicos/src/main/java/ATV06_AreaCirculo/AreaCirculo.java
@@ -1,5 +1,22 @@
 package ATV06_AreaCirculo;
 
+import java.util.Scanner;
+
 public class AreaCirculo {
     
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        //Calculo da area do circulo, onde area = 2 * pi * R
+
+        double area, raio;
+
+        System.out.println("Insira o valor do Raio: ");
+        raio = scanner.nextDouble();
+
+        area = 2 * Math.PI * raio;
+
+        System.out.printf("A área é: %.4f\n", area);
+
+        scanner.close();
+    }
 }
